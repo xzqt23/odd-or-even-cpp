d0ff911b227238465eb2f7669c42ea6905656f98
xzqt23 <angelo.agualada@gmail.com>
2023-02-09 21:44:16 +0800

    Odd or Even number

---
+++ oddOrEven.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main ()
+{
+
+    int number;
+    std::cout <<"Please enter a number:";
+    std::cin>>number;
+
+    if(number % 2 ==0){
+        std::cout <<"The number you entered is an even number"; 
+    }
+
+    else{
+        std::cout <<"The number you entered is an odd number";  
+    }
+    return 0;
+}
